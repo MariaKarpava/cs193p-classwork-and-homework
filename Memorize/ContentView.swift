e176a391b9fac2676808e818c6ee1027ca6fc414
MariaKarpava <mary.talabaeva@gmail.com>
2023-02-21 19:15:18 +0000

    Change Hello, world style

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,20 +7,17 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, world").foregroundColor(.orange)
+        })
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
