9f49194fd7395d7153f6fb74696cd556782e13f2
MariaKarpava <mary.talabaeva@gmail.com>
2023-02-22 19:33:52 +0000

    Add cards and plus and minus buttons

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,92 @@ import SwiftUI
 
 
 struct ContentView: View {
+    var emojis = ["🐶", "🐭", "🦊", "🐻", "🐼", "🐸", "🐵", "🐥", "🦄", "🐰", "🐷"]
+    var emojiCount = 6
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    
+                    ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    })
+                    
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+    }
+    
+    
+    // Buttons:
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+
+struct CardView: View {
+    var content:String
+    @State var isFaceUp: Bool = true
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, world").foregroundColor(.orange)
-        })
-        .padding(.horizontal)
-        .foregroundColor(.red)
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
     }
+}
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
+
 }
 
 
