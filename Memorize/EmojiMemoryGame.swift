3798c4f048bf1fb07e846612e7f7b5a338103b05
MariaKarpava <mary.talabaeva@gmail.com>
2023-03-10 14:52:22 +0000

    Add six Themes. Add New Game Button what chooses the theme randomly. Add Header with the theme name. Make all curds shuffled so that cards that match are located in random parts of the screen.

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -4,40 +4,63 @@
 //
 //  Created by Maryia Karpava on 02/03/2023.
 //
-
-// Our View Model:
-// Intermidiatory between a Model and a View
-
 import SwiftUI
-
-//func makeCardContent(index: Int) -> String {
-//    return "😃"
-//}
-
-
 
 
 class EmojiMemoryGame: ObservableObject {
-    static let emojis = ["🐶", "🐭", "🦊", "🐻", "🐼", "🐸", "🐵", "🐥", "🦄", "🐰", "🐷", "🐴", "🦉", "🐱", "🐹", "🐻‍❄️", "🐨", "🐤", "🦁", "🐒", "🦋", "🐺"]
+    private var theme: Theme
     
-    static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 4, createCardContent: { pairIndex in
-            EmojiMemoryGame.emojis[pairIndex]
-        })
+    static var themes: Array<Theme> = [
+        Theme(name: "Animals", colour: "green", emojis: ["🐶", "🐭", "🦊", "🐻", "🐼", "🐸", "🐵", "🐥", "🦄", "🐰", "🐷", "🐴", "🦉", "🐱", "🐹", "🐻‍❄️", "🐨", "🐤", "🦁", "🐒", "🦋", "🐺"], numberOfPairsOfCardsToShow: 13),
+        Theme(name: "Vehicles", colour: "red", emojis: ["✈️", "🚁", "🚘", "🚃", "🚇", "🚛", "🛳️", "🚲", "🛴"], numberOfPairsOfCardsToShow: 9),
+        Theme(name: "House", colour: "blue", emojis: ["🛁", "🛏️", "🔑", "🪑", "🧸", "🖼️", "🪞", "🚽", "🛋️"], numberOfPairsOfCardsToShow: 9),
+        Theme(name: "Body", colour: "yellow", emojis: ["🦶🏻", "🦵", "🦷", "👅", "👄", "👂", "👃", "👁️", "🫀"], numberOfPairsOfCardsToShow: 9),
+        Theme(name: "Clothes", colour: "brown", emojis: ["👕", "👖", "👗", "🩱", "👘", "👠", "🥾", "👒", "👙"], numberOfPairsOfCardsToShow: 9),
+        Theme(name: "Fruits", colour: "orange", emojis: ["🍏", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🫐"], numberOfPairsOfCardsToShow: 9)
+    ]
+    
+    var themeName: String {
+        theme.name
     }
     
-   @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    @Published private var model: MemoryGame<String>
 
     var cards: Array<MemoryGame<String>.Card> {
         model.cards
+    }
+    
+    init() {
+        theme = EmojiMemoryGame.themes.randomElement()!
+        theme.emojis.shuffle()
+        model = EmojiMemoryGame.createMemoryGame(theme: theme)
+    }
+    
+    
+    static func createMemoryGame(theme: Theme) -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: theme.numberOfPairsOfCardsToShow, createCardContent: { pairIndex in
+            return theme.emojis[pairIndex]
+        })
     }
     
     // MARK: - Intent(s)
     
     func choose(_ card: MemoryGame<String>.Card) {
         model.choose(card)
+//        objectWillChange.send()
+    }
+    
+    func newGame() {
+        theme = EmojiMemoryGame.themes.randomElement()!
+        theme.emojis.shuffle()
+        model = EmojiMemoryGame.createMemoryGame(theme: theme)
     }
     
 }
 
 
+
+struct Previews_EmojiMemoryGame_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
